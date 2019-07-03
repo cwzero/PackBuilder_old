cb34874af8b43a29574b13a59e00195a12626ec9
Eric Gjerde <gjerdeeric@gmail.com>
2019-07-03 08:50:40 -0500

    Fix Spaces?

--- src/main/java/com/liquidforte/packbuilder/util/DownloadHelper.java
+++ src/main/java/com/liquidforte/packbuilder/util/DownloadHelper.java
@@ -1,5 +1,6 @@
 package com.liquidforte.packbuilder.util;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -9,18 +10,18 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class DownloadHelper {
 	public void download(String url, Path file) throws IOException {
-		String originalLocation = url.replace(' ', '+');
+		String originalLocation = url;
+		System.out.println("Original Location: " + originalLocation);
 
 		HttpsURLConnection conn = (HttpsURLConnection) new URL(originalLocation).openConnection();
 		conn.setInstanceFollowRedirects(false);
 		conn.connect();
 
-		String newLocation = conn.getHeaderField("Location").replace(' ', '+');
+		String newLocation = conn.getHeaderField("Location").replace(" ", "%20");
+		System.out.println("New Location: " + newLocation);
+		
+		BufferedInputStream in = new BufferedInputStream(new URL(newLocation).openStream());
 
-		conn = (HttpsURLConnection) new URL(newLocation).openConnection();
-		conn.setInstanceFollowRedirects(false);
-		conn.connect();
-
-		Files.copy(conn.getInputStream(), file);
+		Files.copy(in, file);
 	}
 }
